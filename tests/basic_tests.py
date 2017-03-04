ded9588ac6d7f5d6424f7ee8bde43909620f7c87
Łukasz Harasimowicz <dev@harnash.eu>
2017-03-04 23:18:47 +0100

    Initial tests framework

---
+++ tests/basic_tests.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import os
+from contextlib import contextmanager
+from cookiecutter.utils import rmtree
+
+
+@contextmanager
+def temporary_project(cookies, *args, **kwargs):
+    """
+    Delete the temporal directory that is created when executing the tests
+    :param cookies: pytest_cookies.Cookies
+    """
+    result = cookies.bake(*args, **kwargs)
+    try:
+        yield result
+    finally:
+        rmtree(str(result.project))
+
+def test_envrc(cookies):
+    """
+    Test if the .envrc file was created successfully
+    """
+
+    extra_context = {'project_name': 'environ'}
+
+    with temporary_project(cookies, extra_context=extra_context) as result:
+        envrc_file = result.project.join('.envrc')
+        lines = [x.strip() for x in envrc_file.readlines(cr=False)]
+        assert 'ENVIRON_SECRET_KEY' in lines
+        assert 'ENVIRON_DATABASE_URL' in lines
